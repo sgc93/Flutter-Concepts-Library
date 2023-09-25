91c7bf207e1d55bde5d714b4253e56e558f9f64b
sgc93 <smachewgedefaw@gmail.com>
2023-09-25 12:53:16 -0900

    add mixin to validate user input

---
+++ lib/login_form/mixins/validate_mixin.dart
@@ -0,0 +1,45 @@
+mixin ValidateMixin {
+  String? validateEmailField(String? value) {
+    if (!isEmpty(value)) {
+      return 'Email can\'t be empty!';
+    } else if (!isValid(value)) {
+      return 'Enter correct email format!';
+    } else {
+      return null;
+    }
+  }
+
+  bool isValid(String? value) {
+    if (value!.contains('@')) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  String? validatePasswordField(String? value) {
+    if (!isEmpty(value)) {
+      return 'Password can\'t be empty!';
+    } else if (!hasLength(value)) {
+      return 'Password length must be >= 6.';
+    } else {
+      return null;
+    }
+  }
+
+  bool hasLength(String? value) {
+    if (value!.length < 6) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  bool isEmpty(String? value) {
+    if (value?.isEmpty ?? true) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
