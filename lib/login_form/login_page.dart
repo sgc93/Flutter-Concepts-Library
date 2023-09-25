31b2b1492bcc7038ba3cdf62da66827329ee932a
sgc93 <smachewgedefaw@gmail.com>
2023-09-25 08:16:42 -0900

    recode with functional way

--- lib/login_form/login_page.dart
+++ lib/login_form/login_page.dart
@@ -26,7 +26,6 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     late double deviceWidth = MediaQuery.of(context).size.width;
-
     return Scaffold(
       backgroundColor: Colors.grey[300],
       body: SafeArea(
@@ -34,143 +33,162 @@ class _LoginPageState extends State<LoginPage> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              // logo
-              const SizedBox(height: 50),
-              const Icon(
-                Icons.lock,
-                size: 100,
-              ),
-              // some kind of text
-              const SizedBox(height: 50),
-              Text(
-                'Come Back, Keep in touch!',
-                style: TextStyle(
-                  color: Colors.grey[700],
-                  fontSize: 16,
-                ),
-              ),
-              const SizedBox(height: 25),
-
-              // username field
-              Field(
-                controller: _textController,
-                hintText: 'Email',
-                obscureText: false,
-                keyboardType: _emailKeyboard,
-                iconData: _emailIcon,
-              ),
-              // password field
-              const SizedBox(height: 15),
-              Field(
-                controller: _passwordController,
-                hintText: 'Password',
-                obscureText: true,
-                keyboardType: _passwordKeyboard,
-                iconData: _passwordIcon,
-              ),
-              // forget password?
-              const SizedBox(height: 25),
-
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Text(
-                      'Forgot Password?',
-                      style: TextStyle(
-                        color: Colors.grey.shade600,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-
-              // sign in btn
-              const SizedBox(height: 25),
-              Container(
-                width: deviceWidth,
-                margin: const EdgeInsets.symmetric(horizontal: 25),
-                child: ElevatedButton(
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all(
-                      Colors.black,
-                    ),
-                    padding: MaterialStateProperty.all(
-                      const EdgeInsets.all(20),
-                    ),
-                  ),
-                  onPressed: () {},
-                  child: const Text(
-                    'SignIn',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-              ),
-
-              // or continue with
-
-              const SizedBox(height: 50),
-
+              _horizontalSpacer(50),
+              _logoImage(),
+              _horizontalSpacer(50),
+              _wellcomeText(),
+              _horizontalSpacer(25),
+              _usernameField(),
+              _horizontalSpacer(25),
+              _passwordField(),
+              _horizontalSpacer(25),
+              _resetPassword(),
+              _horizontalSpacer(25),
+              _sighInButton(deviceWidth),
+              _horizontalSpacer(50),
               const Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Text('Or continue with'),
                 ],
               ),
-
-              const SizedBox(height: 50),
-
-              // google + github + linkedIn icon as a button
-
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Button(
-                    imagePath: _googleImgPath,
-                  ),
-                  Button(
-                    imagePath: _githubImgPath,
-                  ),
-                  Button(
-                    imagePath: _linkedInImgPath,
-                  ),
-                ],
-              ),
-
-              // not a member? register now
-              const SizedBox(height: 50),
-
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                mainAxisSize: MainAxisSize.max,
-                children: [
-                  const Text(
-                    'Not a member?',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  TextButton(
-                    onPressed: () {},
-                    child: const Text(
-                      'Register now',
-                      style: TextStyle(
-                        color: Colors.blue,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                ],
-              )
+              _horizontalSpacer(50),
+              _socialMediaLink(),
+              _horizontalSpacer(50),
+              _registerButton(),
             ],
           ),
         ),
       ),
+    );
+  }
+
+  Widget _horizontalSpacer(double height) {
+    return SizedBox(height: height);
+  }
+
+  Widget _logoImage() {
+    return Image.asset(
+      'assets/images/codeSpoon.png',
+      height: 100,
+      width: 200,
+    );
+  }
+
+  Widget _wellcomeText() {
+    return Text(
+      'Come Back, Keep in touch!',
+      style: TextStyle(
+        color: Colors.grey[700],
+        fontSize: 16,
+      ),
+    );
+  }
+
+  Widget _resetPassword() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 25.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          Text(
+            'Forgot Password?',
+            style: TextStyle(
+              color: Colors.grey.shade600,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _sighInButton(double deviceWidth) {
+    return Container(
+      width: deviceWidth,
+      margin: const EdgeInsets.symmetric(horizontal: 25),
+      child: ElevatedButton(
+        style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all(
+            Colors.black,
+          ),
+          padding: MaterialStateProperty.all(
+            const EdgeInsets.all(20),
+          ),
+        ),
+        onPressed: () {},
+        child: const Text(
+          'SignIn',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _socialMediaLink() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Button(
+          imagePath: _googleImgPath,
+        ),
+        Button(
+          imagePath: _githubImgPath,
+        ),
+        Button(
+          imagePath: _linkedInImgPath,
+        ),
+      ],
+    );
+  }
+
+  Widget _registerButton() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      mainAxisSize: MainAxisSize.max,
+      children: [
+        const Text(
+          'Not a member?',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        TextButton(
+          onPressed: () {},
+          child: const Text(
+            'Register now',
+            style: TextStyle(
+              color: Colors.blue,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _usernameField() {
+    return Field(
+      controller: _textController,
+      hintText: 'Email',
+      labelText: 'Email',
+      obscureText: false,
+      keyboardType: _emailKeyboard,
+      iconData: _emailIcon,
+    );
+  }
+
+  Widget _passwordField() {
+    return Field(
+      controller: _passwordController,
+      hintText: 'Password',
+      labelText: 'Password',
+      obscureText: true,
+      keyboardType: _passwordKeyboard,
+      iconData: _passwordIcon,
     );
   }
 }
