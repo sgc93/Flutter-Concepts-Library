940b1fe9f031a77f6612289818478f704549e6b6
sgc93 <smachewgedefaw@gmail.com>
2023-10-06 10:52:00 -0900

    add responsive desktop layout components

--- lib/responsiveness/layoutBuilder/layouts/desktop_body.dart
+++ lib/responsiveness/layoutBuilder/layouts/desktop_body.dart
@@ -6,10 +6,97 @@ class DesktopBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.deepPurple[300],
+      backgroundColor: Colors.deepPurple[200],
       appBar: AppBar(
         leading: const Icon(Icons.desktop_windows),
         title: const Text('DESKTOP LAYOUT'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row(
+          children: [
+            // First column
+            Expanded(
+              child: Column(
+                children: [
+                  // youtube video
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: AspectRatio(
+                      aspectRatio: 16 / 9,
+                      child: Container(
+                        color: Colors.deepPurple[400],
+                        child: const Center(
+                          child: Text(
+                            textAlign: TextAlign.center,
+                            'Y O U T U B E - V I D E O \n container',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 25,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+
+                  // comment section & recommended videos
+                  Expanded(
+                    child: ListView.builder(
+                      itemCount: 8,
+                      itemBuilder: (context, index) {
+                        return Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Container(
+                            color: Colors.deepPurple[300],
+                            height: 120,
+                            child: const Center(
+                              child: Text(
+                                'comment \n container',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 16,
+                                ),
+                                textAlign: TextAlign.center,
+                              ),
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                  )
+                ],
+              ),
+            ),
+
+            // second column
+            SizedBox(
+              width: 200,
+              child: ListView.builder(
+                itemCount: 8,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      color: Colors.deepPurple[300],
+                      height: 120,
+                      child: const Center(
+                        child: Text(
+                          'comment \n container',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 16,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
