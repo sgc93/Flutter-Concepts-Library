3618e2407d1e97ad98a299d486621fe2483cec33
sgc93 <smachewgedefaw@gmail.com>
2023-10-06 02:30:22 -0900

    define list of tabs with TabBar widget

--- lib/working_with_tabs/whatsapp_appBar.dart
+++ lib/working_with_tabs/whatsapp_appBar.dart
@@ -5,15 +5,39 @@ class WhatsAppTab extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.green,
-        title: const Text('WhatsApp'),
-        actions: const [
-          Icon(Icons.search),
-          Icon(Icons.more_vert),
-        ],
+    return DefaultTabController(
+      initialIndex: 0,
+      length: 4,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.green,
+          title: const Text('WhatsApp'),
+          actions: const [
+            Icon(Icons.search),
+            Icon(Icons.more_vert),
+          ],
+          bottom: TabBar(
+            tabs: _getTabs(),
+          ),
+        ),
       ),
     );
+  }
+
+  List<Tab> _getTabs() {
+    return const [
+      Tab(
+        icon: Icon(Icons.camera_alt),
+      ),
+      Tab(
+        child: Text('CHATS'),
+      ),
+      Tab(
+        child: Text('STATUS'),
+      ),
+      Tab(
+        child: Text('CALLS'),
+      ),
+    ];
   }
 }
