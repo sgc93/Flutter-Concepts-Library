b938065bda088516efeb60c134840517a2e88cdf
sgc93 <smachewgedefaw@gmail.com>
2023-10-22 14:37:33 -0900

    wrap the root widget with Provider widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_concepts/dark_light_them/provider/theme_provider.dart';
 import 'package:flutter_concepts/dark_light_them/screen.dart';
 import 'package:flutter_concepts/dark_light_them/theme/my_theme.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(ChangeNotifierProvider(
+    create: (context) => ThemeProvider(),
+    child: const MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
@@ -15,6 +20,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Concepts Library',
       theme: MyTheme.lightTheme,
+      darkTheme: MyTheme.darkTheme,
       home: const HomeScreen(),
     );
   }
